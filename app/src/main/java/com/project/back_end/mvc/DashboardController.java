d99553a2252a0d1e63c15eb0c50305ec3ce81be9
MainConsole <OverHive@users.noreply.github.com>
2025-07-11 09:02:05 +0100

    Setup Thymeleaf and created controller

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,15 +1,24 @@
 package com.project.back_end.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+@Controller
 public class DashboardController {
 
 // 1. Set Up the MVC Controller Class:
 //    - Annotate the class with `@Controller` to indicate that it serves as an MVC controller returning view names (not JSON).
 //    - This class handles routing to admin and doctor dashboard pages based on token validation.
 
+boolean validateToken(String token, String role)
+{
+return token == role;
+}
 
 // 2. Autowire the Shared Service:
 //    - Inject the common `Service` class, which provides the token validation logic used to authorize access to dashboards.
-
+@Autowired
 
 // 3. Define the `adminDashboard` Method:
 //    - Handles HTTP GET requests to `/adminDashboard/{token}`.
@@ -17,6 +26,18 @@ public class DashboardController {
 //    - Validates the token using the shared service for the `"admin"` role.
 //    - If the token is valid (i.e., no errors returned), forwards the user to the `"admin/adminDashboard"` view.
 //    - If invalid, redirects to the root URL, likely the login or home page.
+@GetMapping("/adminDashboard/{token}")
+String adminDashboard(@PathVariable("token") String token)
+{
+ if (validateToken(token,"admin"))
+ {
+    return "admin/adminDashboard";
+ }
+ else
+ {
+    return "/";
+ }
+}
 
 
 // 4. Define the `doctorDashboard` Method:
@@ -26,5 +47,16 @@ public class DashboardController {
 //    - If the token is valid, forwards the user to the `"doctor/doctorDashboard"` view.
 //    - If the token is invalid, redirects to the root URL.
 
-
+@GetMapping("/adminDashboard/{token}")
+String doctorDashboard(@PathVariable("token") String token)
+{
+ if (validateToken(token,"doctor"))
+ {
+    return "doctor/doctorDashboard";
+ }
+ else
+ {
+    return "/";
+ }
+}
 }
